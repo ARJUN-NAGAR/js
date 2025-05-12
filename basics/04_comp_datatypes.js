aa485c93d33f99291bfad0a11bdc8a4667a0646d
ARJUN-NAGAR <145248689+ARJUN-NAGAR@users.noreply.github.com>
2025-05-12 16:58:23 +0000

    container config done

--- basics/04_comp_datatypes.js
+++ basics/04_comp_datatypes.js
@@ -1,1 +1,42 @@
 
+// let score=33
+// score=-score
+// console.log(score)  // -33
+// console.log(typeof score) // number
+
+
+// console.log(3+2)  5
+// console.log(3-2)  1
+// console.log(3*2)  6
+// console.log(3/2)  1.5; result in float value
+// console.log(3**2) power value 9
+// console.log(3+-2) 3+(-2)=1 
+// console.log(3%2)  1
+// console.log(3&2)  2 solved in binary multiplier
+
+
+// console.log("1"+2)           //      12  in string
+// console.log(typeof ("1"+2))  
+// console.log(1+"2")           //      12 in string
+// console.log(typeof (1+"2"))  
+// console.log("1"+2+2)         //      122 in string
+// console.log(typeof ("1"+2+2))  
+// console.log(1+2+"2")         //      32 in string
+// console.log(typeof (1+2+"2"));
+
+
+// console.log(+true)  1
+// console.log(+false) 0
+
+// console.log(2>1)    true
+// console.log(2<1)    false
+// console.log(2>=1)   true
+// console.log(2==1)   false
+
+// console.log(null>0)      comparison; convert to number i.e 0>0 false
+// console.log(null<0)      0<0 false
+// console.log(null>=0)     0>=0 true
+// console.log(null<=0)     0<=true
+
+console.log("2"==2)   //checks for data here gives true
+console.log("2"===2) //checks for type of datatypes here gives false
---
+++ basics/04_summary.js
@@ -0,0 +1,29 @@
+
+// datatypes
+    // primitive
+        // 7 types
+            // String
+            // Number
+            // Null
+            // Boolean
+            // Undefined
+            // Symbol
+            // Bigint
+    
+    
+    // reference type or non primitive
+        // Arrays
+        // Objects
+        // Function
+
+
+// JavaScript is a dynamica type language. here you don't need to declare variable
+const id=Symbol("11")
+const id1=Symbol("11")
+
+console.log(id===id1)
+/*const num=9234567891254354857443456789009876543212345678098765432345678998765433456783678765444567
+const num1=46644646466464646477777777777777777777777777777777777777777777777777777777777777777777777777777777777777n*/
+
+console.log(num)
+console.log(num1)
