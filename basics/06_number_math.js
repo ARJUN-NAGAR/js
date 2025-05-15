7a09a493ecfb5a66215ba3a77bb0ccbed5c6744c
ARJUN-NAGAR <145248689+ARJUN-NAGAR@users.noreply.github.com>
2025-05-15 09:47:42 +0000

    container config done

---
+++ basics/06_number_math.js
@@ -0,0 +1,30 @@
+
+// const score=100
+// console.log(score)
+// const balance=new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.length)
+// console.log(balance.toFixed())
+// console.log(balance.toFixed(2))
+// const othernum=11123.89
+// console.log(othernum.toPrecision(3))
+const num=100000
+
+console.log(num.toLocaleString('en-IN'))
+
+//-------------------------- maths --------------------------//
+
+// console.log(Math)
+// console.log(Math.abs(-10))
+// console.log(Math.round(4.9))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(6.9))
+// console.log(Math.max(2,3,4,5,6))
+// console.log(Math.min(-11,-1,-1111111111111,2,3,4,5))
+// console.log(Math.random())// get the value between 0 and 1
+// console.log((Math.random()*10)+1)// avoid the case of 0.0444
+
+const max=20
+const min=10
+console.log(Math.floor(Math.random()*(max-min+1)+min))
