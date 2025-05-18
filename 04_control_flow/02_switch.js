d9e14457e07f52c2bb4835ab290b80f07b52b8a1
ARJUN-NAGAR <145248689+ARJUN-NAGAR@users.noreply.github.com>
2025-05-18 16:29:10 +0000

    container config done

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,48 @@
+
+// switch(key) {
+//     case value:
+//         break;
+//     case value:
+//         break;
+//     default:
+//         break; 
+// }
+// const month=3
+// switch(month){
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("Febrary")
+//         break;
+//     case 3:
+//         console.log("March")
+//         break;
+//     case 4:
+//         console.log("Aipril")
+//         break;
+//     case 5:
+//         console.log("May")
+//         break;
+// }
+
+const month = "aug"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
